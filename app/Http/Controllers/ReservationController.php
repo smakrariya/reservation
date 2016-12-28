75ec191261c14e8e4099dab3e941cacc4f486a46
smakrariya <smakrariya@gmail.com>
2016-12-28 11:52:36 +0530

    backend

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -17,53 +17,62 @@ class ReservationController extends Controller
             }
         }
         $seatBook = 6;
+        $seatBooked = 0;
         if ($seatBook <= 7 && $seatBook > 0) {
             for ($i = 11; $i >= 0; $i--) {
                 $counter = 0;
-                for ($j = 0; $j < 7; $j++) {
-                    if($seat[$i][$j] == 0)
-                        $counter++;
-                    if($counter == $seatBook){
-                        for($k = 0 ; $k < 7; $k++){
-                            if($seat[$i][$k] == 0)
-                                break;
-                        }
-                        $loopLimit = $k+$seatBook;
-                        while ($k < $loopLimit){
-                            $seat[$i][$k] = 1;
-                            $k++;
+                if($seatBooked) {
+                    for ($j = 0; $j < 7; $j++) {
+                        if ($seat[$i][$j] == 0)
+                            $counter++;
+                        if ($counter == $seatBook) {
+                            for ($k = 0; $k < 7; $k++) {
+                                if ($seat[$i][$k] == 0)
+                                    break;
+                            }
+                            $loopLimit = $k + $seatBook;
+                            while ($k < $loopLimit) {
+                                $seat[$i][$k] = 1;
+                                $k++;
+                                $seatBooked = 1;
+                            }
+
                         }
 
                     }
-
                 }
             }
         }
         var_dump($seat);
+        echo '<br />';
 
         $seatBook = 4;
+        $seatBooked = 0;
         if ($seatBook <= 7 && $seatBook > 0) {
             for ($i = 11; $i >= 0; $i--) {
                 $counter = 0;
-                for ($j = 0; $j < 7; $j++) {
-                    if($seat[$i][$j] == 0)
-                        $counter++;
-                    if($counter == $seatBook){
-                        for($k = 0 ; $k < 7; $k++){
-                            if($seat[$i][$k] == 0)
-                                break;
-                        }
-                        $loopLimit = $k+$seatBook;
-                        while ($k < $loopLimit){
-                            $seatBook[$i][$k] = 1;
-                            $k++;
+                if($seatBooked) {
+                    for ($j = 0; $j < 7; $j++) {
+                        if ($seat[$i][$j] == 0)
+                            $counter++;
+                        if ($counter == $seatBook) {
+                            for ($k = 0; $k < 7; $k++) {
+                                if ($seat[$i][$k] == 0)
+                                    break;
+                            }
+                            $loopLimit = $k + $seatBook;
+                            while ($k < $loopLimit) {
+                                $seatBook[$i][$k] = 1;
+                                $k++;
+                            }
+
                         }
 
                     }
-
                 }
             }
         }
+        echo '<br />';
         var_dump($seat);
 
     }

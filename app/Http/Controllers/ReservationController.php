fa8f8559beaf5a1552e8ab9de9f01405fb4179fa
smakrariya <smakrariya@gmail.com>
2016-12-28 12:37:51 +0530

    backend

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -17,7 +17,7 @@ class ReservationController extends Controller
             }
         }
 
-        for($i = 0; $i <= 6; $i++){
+        for($i = 0; $i <= 11; $i++){
             $seat = $this->SeatBooking($seat, rand(1, 7));
             $this->printArray($seat);
         }
